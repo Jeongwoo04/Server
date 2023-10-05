693e22a745134ca38702e148647e2ca1a2ea0d30
jeson <sjo7592@gmail.com>
2023-10-05 15:46:28 +0900

    초기 커밋.

---
+++ ServerCore/CorePch.h
@@ -0,0 +1,18 @@
+#pragma once
+
+// Client나 GameServer에서 참조하기 원하는 것들
+
+#include "Types.h"
+#include <vector>
+#include <list>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+
+#include <iostream>
+using namespace std;
+
+void HelloWorld();
