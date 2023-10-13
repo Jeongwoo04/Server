891f6577fa384c9729f1ef0e86cfe2b6c9bb293d
jeson <sjo7592@gmail.com>
2023-10-14 00:51:48 +0900

    python jinja2를 이용한 패킷 자동화 추가 / Tools 및 폴더 정리

--- DummyClient/DummyClient.cpp
+++ DummyClient/DummyClient.cpp
@@ -3,7 +3,7 @@
 #include "Service.h"
 #include "Session.h"
 #include "BufferReader.h"
-#include "ClientPacketHandler.h"
+#include "ServerPacketHandler.h"
 
 char sendData[] = "Hello World";
 
@@ -22,7 +22,11 @@ public:
 
 	virtual void OnRecvPacket(BYTE* buffer, int32 len) override
 	{
-		ClientPacketHandler::HandlePacket(buffer, len);
+		PacketSessionRef session = GetPacketSessionRef();
+		PacketHeader* header = reinterpret_cast<PacketHeader*>(buffer);
+
+		// TODO : packetId 대역 체크
+		ServerPacketHandler::HandlePacket(session, buffer, len);
 	}
 
 
@@ -39,6 +43,8 @@ public:
 
 int main()
 {
+	ServerPacketHandler::Init();
+
 	this_thread::sleep_for(1s);
 
 	ClientServiceRef service = MakeShared<ClientService>(
